d4508cbfc48090a4969d37af73f9a06412425730
varnel <varnel.tientcheu@ensae.fr>
2026-03-31 00:00:30 +0000

    Question 2

--- Work.R
+++ Work.R
@@ -27,42 +27,56 @@ df1 <- df %>%
   ) %>%
   droplevels()
 
-  # a) Modèle LOG 
-# Modèle 1 : Effet brut de l'origine
-m1 <- lm_robust(logsalhoraire~ origine, data = df1)
-# Modèle 2 : Ajout des variables démographiques et cycle de vie
-m2 <- lm_robust(logsalhoraire ~ origine + age + age2 + homme, data = df1)
-# Modèle 3 : Ajout des contrôles géographiques (Effets de structure)
-m3 <- lm_robust(logsalhoraire ~ origine + age + age2 + homme + region + tuu_r, 
-                data = df1)
-# Modèle 4 : Modèle complet avec Capital Humain (Blocage du canal éducation)
-m4 <- lm_robust(logsalhoraire ~ origine + age + age2 + homme + region + tuu_r + education, 
-                data = df1)
-# Modèle 5 : Modèle complet avec bad controls
-m5 <- lm_robust(logsalhoraire ~ origine + age + age2 + homme + region + tuu_r + csp_actif + contrat+ education, 
-                data = df1)
 
-# 3. Tableau récapitulatif
-list(
-  "Modèle (1)" = m1, 
-  "Modèle (2)" = m2, 
-  "Modèle (3)" = m3, 
-  "Modèle (4)" = m4,
-  "Modèle (5)" = m5
-) %>%
+regression <- function(y, data = df1){
+  
+  # On convertit l'argument en caractère si c'est un nom non cité
+  y <- deparse(substitute(y))
+  
+  # Définition des listes de variables explicatives
+  rhs1 <- "origine"
+  rhs2 <- c(rhs1, "age", "age2", "homme")
+  rhs3 <- c(rhs2, "region", "tuu_r")
+  rhs4 <- c(rhs3, "education")
+  rhs5 <- c(rhs4, "csp_actif", "contrat")
+  
+  # Estimation des modèles avec reformulate()
+  m1 <- lm_robust(reformulate(rhs1, y), data = data)
+  m2 <- lm_robust(reformulate(rhs2, y), data = data)
+  m3 <- lm_robust(reformulate(rhs3, y), data = data)
+  m4 <- lm_robust(reformulate(rhs4, y), data = data)
+  m5 <- lm_robust(reformulate(rhs5, y), data = data)
+  
+  # On regroupe pour le tableau
+  models <- list(
+    "modèle(1)" = m1, 
+    "modèle(2)" = m2, 
+    "modèle(3)" = m3, 
+    "modèle(4)" = m4, 
+    "modèle(5)" = m5
+  )
+  
+  # Rendu du tableau
   modelsummary(
+    models,
     stars = TRUE,
     coef_omit = "Intercept",
     gof_omit = "AIC|BIC|Log.Lik|F|Std.Errors",
-    title = "Évolution des coefficients du salaire horaire",
-    notes = "Écarts-types robustes (HC2) entre parenthèses."
+    title = paste("Analyse de la variable :", y),
+    notes = "Écarts-types robustes (HC2)."
   )
+  
+}
 
+  # a) Modèle LOG 
+regression(logsalhoraire)
+# a) Modèle en niveau 
+regression(salhoraire)
 
 #------------------------------------------------------------
 
 boxplot(
-  logsalhoraire ~ csp_actif,
+  logsalhoraire ~ origine,
   data = df,
   col = "lightblue",
   main = "Distribution des salaires selon le niveau d'éducation",
@@ -71,7 +85,7 @@ boxplot(
 
 
  # Exemple
-table_data <- table(df$csp_actif, df$education)
+table_data <- table(df1$education, df1$origine)
 
 barplot(
   table_data,
@@ -80,5 +94,3 @@ barplot(
   legend = TRUE,
   main = "Interaction entre deux variables qualitatives"
 )
-
-hist(df$age)
